b2c89da6d8b5b2aa5bdd6346e91652e5e67b691b
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-06-20 23:25:31 +0800

    feat: 项目配置 1

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,20 @@
-<script setup></script>
-
+<script setup>
+const handleClickMini = () => {
+  AMessage.info('hello isboyjc, click mini AButton!')
+}
+</script>
 <template>
-  <div>hello marcus, This is toolsdog home page!</div>
-</template>
+  <div>hello isboyjc, This is toolsdog home page!</div>
+  <a-space>
+    <a-button type="primary" size="mini" @click="handleClickMini"> Mini </a-button>
+    <a-button type="primary" size="small">Small</a-button>
+    <a-button type="primary">Medium</a-button>
+    <a-button type="primary" size="large">Large</a-button>
+  </a-space>
 
-<style scoped></style>
+  <!-- 这里 -->
+  <HelloWorld />
+  <div>hello</div>
+  <div>hello</div>
+</template>
+<style scoped lang="scss"></style>
